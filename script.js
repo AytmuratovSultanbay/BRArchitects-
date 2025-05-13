f7a0625e6d02259bcec67add312b6f2100945f03
AytmuratovSultanbay <ajtmuratovsultanbaj@gmail.com>
2025-05-13 09:15:40 +0300

    commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var telegram_bot_id = "7523496541:AAHjWWsk4D3E9UHG7aHNeCUFow9P4_POrx4"; 
+var chat_id = 2567421785; 
+var u_name, email, message;
+var ready = function() {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Ismi: " + u_name + "\nEmail: " + email + "\nIzoh: " + message;
+};
+var sendtelegram = function() {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"             
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message,
+        })
+    };
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
